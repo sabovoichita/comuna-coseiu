ddd9b990950222173693eb607150ef881da38e20
Voichita Maria <sabo_voichita@yahoo.com>
2024-06-28 09:56:53 +0200

    add images-slideshow

--- index.js
+++ index.js
@@ -26,22 +26,98 @@ function createMainLayout() {
         <li><a href ="#">Galerie</a></li> 
         </ul>
       </div>
-    <div class = "main hide">
-      <h2>Galerie Foto</h2></div>
-      <div class="foto">
-        <img src="images/20190111_082942.jpg" >
-        <img src="images/20190106_120941a.jpg" >
-        <img src="images/20181221_093455a.jpg" >
-        <img src="images/20190111_082940.jpg" >
-        <img src="images/20210803_085701.jpg" >
-      </div>
-    </div>
+
+<div class="slideshow-container" >
+
+  <div class="mySlides fade" >
+    <div class="numbertext">1 / 6</div>
+     <img src="images/20190110_084654a.jpg" >
+    <div class="text">Coseiu iarna</div>
+  </div>
+  <div class="mySlides fade" >
+    <div class="numbertext">2 / 6</div>
+    <img src="images/20190106_120941.jpg"  >
+    <div class="text">Coseiu iarna</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">3 / 6</div>
+    <img src="images/20181221_093455.jpg" >
+    <div class="text">Coseiu iarna</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">4 / 6</div>
+    <img src="images/20190111_082942a.jpg" >
+    <div class="text">Coseiu iarna</div>
+  </div>
+
+   <div class="mySlides fade">
+    <div class="numbertext">5 / 6</div>
+    <img src="images/20190110_084701a.jpg" >
+    <div class="text">Coseiu iarna</div>
+  </div>
+   
+   <div class="mySlides fade">
+    <div class="numbertext">6 / 6</div>
+   <img src="images/20210803_085701.jpg" >
+    <div class="text">Coseiu toamna</div>
+  </div>
+
+  <!-- Next and previous buttons -->
+  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+<!-- The dots/circles -->
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span>
+  <span class="dot" onclick="currentSlide(2)"></span>
+  <span class="dot" onclick="currentSlide(3)"></span>
+  <span class="dot" onclick="currentSlide(4)"></span>
+  <span class="dot" onclick="currentSlide(5)"></span>
+  <span class="dot" onclick="currentSlide(6)"></span>
+</div>
 `;
+}
+
+let slideIndex = 1;
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
 
 function initEvents() {
   console.log("here");
   const insertH1 = ($("body").innerHTML = createMainLayout());
   console.log(insertH1);
+  showSlides(slideIndex);
 }
 initEvents();
